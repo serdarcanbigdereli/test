f0180e8072c0e77ea4ac423c1de0e35c2b096c16
serdarcanbigdereli <serdarcanbuyukdereli@gmail.com>
2016-12-12 13:23:58 +0200

    Add files via upload

---
+++ build-Sqlite_DB-Desktop-Debug/ui_login.h
@@ -0,0 +1,132 @@
+/********************************************************************************
+** Form generated from reading UI file 'login.ui'
+**
+** Created by: Qt User Interface Compiler version 5.6.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_LOGIN_H
+#define UI_LOGIN_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Login
+{
+public:
+    QWidget *centralWidget;
+    QVBoxLayout *verticalLayout_2;
+    QGroupBox *groupBox;
+    QVBoxLayout *verticalLayout;
+    QLineEdit *lineEdit_username;
+    QLabel *label_2;
+    QLineEdit *lineEdit_password;
+    QLabel *label_3;
+    QPushButton *pushButton;
+    QLabel *label;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *Login)
+    {
+        if (Login->objectName().isEmpty())
+            Login->setObjectName(QStringLiteral("Login"));
+        Login->resize(609, 321);
+        centralWidget = new QWidget(Login);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        verticalLayout_2 = new QVBoxLayout(centralWidget);
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        groupBox = new QGroupBox(centralWidget);
+        groupBox->setObjectName(QStringLiteral("groupBox"));
+        verticalLayout = new QVBoxLayout(groupBox);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        lineEdit_username = new QLineEdit(groupBox);
+        lineEdit_username->setObjectName(QStringLiteral("lineEdit_username"));
+
+        verticalLayout->addWidget(lineEdit_username);
+
+        label_2 = new QLabel(groupBox);
+        label_2->setObjectName(QStringLiteral("label_2"));
+
+        verticalLayout->addWidget(label_2);
+
+        lineEdit_password = new QLineEdit(groupBox);
+        lineEdit_password->setObjectName(QStringLiteral("lineEdit_password"));
+
+        verticalLayout->addWidget(lineEdit_password);
+
+        label_3 = new QLabel(groupBox);
+        label_3->setObjectName(QStringLiteral("label_3"));
+
+        verticalLayout->addWidget(label_3);
+
+        pushButton = new QPushButton(groupBox);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+
+        verticalLayout_2->addWidget(groupBox);
+
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+
+        verticalLayout_2->addWidget(label);
+
+        Login->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(Login);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 609, 28));
+        Login->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(Login);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        Login->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(Login);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        Login->setStatusBar(statusBar);
+
+        retranslateUi(Login);
+
+        QMetaObject::connectSlotsByName(Login);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *Login)
+    {
+        Login->setWindowTitle(QApplication::translate("Login", "Login", 0));
+        groupBox->setTitle(QApplication::translate("Login", "singing", 0));
+        label_2->setText(QApplication::translate("Login", "username", 0));
+        label_3->setText(QApplication::translate("Login", "password", 0));
+        pushButton->setText(QApplication::translate("Login", "PushButton", 0));
+        label->setText(QApplication::translate("Login", "Status", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Login: public Ui_Login {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_LOGIN_H
